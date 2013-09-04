245070f96f7b5c9f61cd60c2173660469562ae73
Manish M <manish.m@armiasystems.com>
2013-09-04 14:56:33 +0530

    Initial Commit

---
+++ ChitPay/CPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CPAppDelegate.h
+//  ChitPay
+//
+//  Created by Armia on 04/09/13.
+//  Copyright (c) 2013 Armia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CPViewController;
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CPViewController *viewController;
+
+@end
---
+++ ChitPay/CPViewController.m
@@ -0,0 +1,29 @@
+//
+//  CPViewController.m
+//  ChitPay
+//
+//  Created by Armia on 04/09/13.
+//  Copyright (c) 2013 Armia. All rights reserved.
+//
+
+#import "CPViewController.h"
+
+@interface CPViewController ()
+
+@end
+
+@implementation CPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
